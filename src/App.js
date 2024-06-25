35c94c3376ce3a6bad7a21a266b80714b607290b
RichardDavidKaavalan <richard.david@kaavalan.org>
2024-06-25 18:04:18 +0530

    Logo Change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Cyber Check 360 Demo Project again wefwe
+          Hello world
         </a> 
       </header>
     </div>
